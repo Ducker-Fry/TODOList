af7c4ff8890e54661db43f449bb9cfa1b0a05ba7
Ducker-Fry <151835jiaxin@gmail.com>
2025-07-10 11:01:40 +0800

    数据库没有连接成功

--- main.cpp
+++ main.cpp
@@ -7,30 +7,12 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     // 初始化数据库
-    //if (!DatabaseManager::instance().initDatabase())
-    //{
-    //    qDebug() << "Failed to initialize database.";
-    //    return -1;
-    //}
-
-// 检查可用的数据库驱动
-    qDebug() << "Available drivers:" << QSqlDatabase::drivers();
-
-    // 尝试加载MySQL驱动
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("localhost");
-    db.setDatabaseName("todo_db");
-    db.setUserName("root");
-    db.setPassword("112106120Sql@");
-
-    if (!db.open())
+    if (!DatabaseManager::instance().initDatabase())
     {
-        qDebug() << "Failed to open database:" << db.lastError().text();
+        qDebug() << "Failed to initialize database.";
+        return -1;
     }
-    else
-    {
-        qDebug() << "Database opened successfully.";
-    }
+
     MainWindow window;
     window.show();
     return app.exec();
